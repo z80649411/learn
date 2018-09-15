a69089ef90c88517ba49dd7682c5591f8fea3d5b
dengrenbo@outlook.com <Drb.199519>
2018-09-15 14:27:39 +0800

    整合springboot2

--- activiti6-samples/springboot/src/main/java/cn/lucasma/activiti/springboot/springboot/SpringbootApplication.java
+++ activiti6-samples/springboot/src/main/java/cn/lucasma/activiti/springboot/springboot/SpringbootApplication.java
@@ -1,9 +1,10 @@
 package cn.lucasma.activiti.springboot.springboot;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringbootApplication {
 
     public static void main(String[] args) {
