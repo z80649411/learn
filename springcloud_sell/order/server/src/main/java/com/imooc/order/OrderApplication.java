631a3d506efb0b33bc30e50d067f4a0e52e342fa
dengrenbo@outlook.com <Drb.199519>
2018-10-13 15:44:51 +0800

    xx

--- springcloud_sell/order/server/src/main/java/com/imooc/order/OrderApplication.java
+++ springcloud_sell/order/server/src/main/java/com/imooc/order/OrderApplication.java
@@ -3,11 +3,15 @@ package com.imooc.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.imooc.product.client")
+//@EnableFeignClients(basePackages={"com.imooc.product.client"})
+@EnableFeignClients
+@ComponentScan(basePackages={"com.imooc.product.client"})
 public class OrderApplication {
 
 	public static void main(String[] args) {
